8d84a07a7423faf2c29c107581a358800aa02847
Kishore <pashindlakishore@gmail.com>
2017-07-22 22:14:43 +0100

    Summed up values

--- FisrtProject/src/com/qa/ScoreCard.java
+++ FisrtProject/src/com/qa/ScoreCard.java
@@ -6,11 +6,15 @@ public class ScoreCard {
 	public void displayScore()
 	{
 		System.out.println("Data ");
+		int sum = 0;
 		for(int i = 0; i<scores.length; i++)
 		{
 		    
 		    System.out.println("Item "+scores[i]);
+		    sum += scores[i];
 		}
+		
+		System.out.println("Sum is "+sum);
 	}
 
 }
